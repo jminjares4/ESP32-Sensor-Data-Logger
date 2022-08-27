b800e82b9f128c666fd5e99359cfdd7431d04c30
jminjares4 <minjares.jesus.ths@gmail.com>
2022-08-26 20:26:43 -0600

    Added rtc driver

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -13,119 +13,10 @@
 #include "oled/fonts.h"
 
 #include "bmp180.h"
+#include "ds3231.h"
 #include <inttypes.h>
 
 #include "esp_idf_version.h"
-
-/* Dummy struct */
-typedef struct {
-    float temp;
-    int pressure;
-}temp_t;
-
-
-/* Global Queue handle */
-QueueHandle_t myQueue;
-
-#define ONBOARD_LED 2
-#define EXTERNAL_LED 4
-
-void task_one(void *pvParameters)
-{
-   /* Select GPIO pad */
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
-   esp_rom_gpio_pad_select_gpio(ONBOARD_LED);
-#else
-   gpio_pad_select_gpio(ONBOARD_LED);
-#endif
-   /* Set direction as output */
-   gpio_set_direction(ONBOARD_LED, GPIO_MODE_OUTPUT);
-
-   while (1)
-   {
-      /* Toggle onboard led every second */
-      gpio_set_level(ONBOARD_LED, 0);
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-      gpio_set_level(ONBOARD_LED, 1);
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-      /* Print task 1 */
-      printf("Task 1\n");
-   }
-}
-void task_two(void *pvParameters)
-{
-   /* Select GPIO pad */
-
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
-   esp_rom_gpio_pad_select_gpio(EXTERNAL_LED);
-#else
-   gpio_pad_select_gpio(EXTERNAL_LED);
-#endif
-   /* Set direction as output */
-   gpio_set_direction(EXTERNAL_LED, GPIO_MODE_OUTPUT);
-
-   while (1)
-   {
-      /* Toggle external led every 1/2 second */
-      gpio_set_level(EXTERNAL_LED, 0);
-      vTaskDelay(500 / portTICK_PERIOD_MS);
-      gpio_set_level(EXTERNAL_LED, 1);
-      vTaskDelay(500 / portTICK_PERIOD_MS);
-      /* Print task 2 */
-      printf("Task 2\n");
-   }
-}
-void ledTask(void *pvParameters)
-{
-   /* Set led */
-   led_t led = {.pin = 15, .state = OFF};
-
-   /* Enable led */
-   led_enable(&led);
-
-   /* Turn off led */
-   led_off(&led);
-
-   while (1)
-   {
-      /* Toggle led */
-      led_toggle(&led);
-      /* Print state */
-      led.state ? printf("LED is on!\n") : printf("LED is off...\n");
-      /* 100 ms delay */
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-   }
-}
-void buttonTask(void *pvParameters)
-{
-   /* Set button configuration */
-   button_t button;
-   button.pin = 23;
-   button.pull_sel.up = 1;
-   button.func = NULL;
-   /* Initialize button */
-   button_enable(&button);
-
-   /* Set led configuration */
-   led_t extraLed;
-   extraLed.pin = 25;
-   extraLed.state = OFF;
-
-   /* Initialize led */
-   led_enable(&extraLed);
-
-   while (1)
-   {
-      /* Debounce button */
-      vTaskDelay(10 / portTICK_PERIOD_MS);
-      /* Check if button is pressed
-         True:    led on
-         False:   led off
-      */
-      !read_button(&button) ? led_on(&extraLed) : led_off(&extraLed);
-      vTaskDelay(100 / portTICK_PERIOD_MS); /* 100 ms delay*/
-   }
-}
 
 void lcdTask(void *pvParameters)
 {
@@ -161,9 +52,7 @@ void lcdTask(void *pvParameters)
 }
 
 void bmp180Task(void *pvParameters){
-
-   ESP_ERROR_CHECK(i2cdev_init());
-
+   
    bmp180_dev_t dev; 
    memset(&dev, 0, sizeof(bmp180_dev_t));
 
@@ -181,58 +70,72 @@ void bmp180Task(void *pvParameters){
         esp_err_t res = bmp180_measure(&dev, &temp, &pressure, BMP180_MODE_STANDARD);
         if (res != ESP_OK)
             printf("Could not measure: %d\n", res);
-        else{
+        else
             /* float is used in printf(). you need non-default configuration in
              * sdkconfig for ESP8266, which is enabled by default for this
              * example. see sdkconfig.defaults.esp8266
              */
             printf("Temperature: %.2f degrees Celsius; Pressure: %" PRIu32 " Pa\n", temp, pressure);
-            temp_t data = {.temp = temp, .pressure= pressure};
-            xQueueSendToBack(myQueue, &data, 0);
-        }
-
+        
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
-void ssd1306Task(void *pvParameters){
-   
-   SSD1306_Init();
-   
-   printf("SSD1306 Initialized...\n");
+void rtcTask(void *pvParameters){
 
-   char str[16];
 
-   temp_t dummy_data = {.temp = 0.0, .pressure = 0};
+      i2c_dev_t dev;
+      memset(&dev, 0, sizeof(i2c_dev_t));
 
-   while(1){
-      if(xQueueReceive(myQueue, &dummy_data, (TickType_t)100) == pdPASS){
+      gpio_num_t i2c_sda = 21;
+      gpio_num_t i2c_scl = 22;
 
-         SSD1306_GotoXY(10,10);
-         SSD1306_Clear();
-         vTaskDelay(10);
-         float temp_f = (dummy_data.temp * 9/5) + 32;
-         sprintf(str,"T:%.2f F", temp_f);
-         SSD1306_Puts(str,&Font_11x18,1);
-         SSD1306_GotoXY(10,40);
-         sprintf(str,"P:%d Pa",dummy_data.pressure);
-         SSD1306_Puts(str,&Font_11x18,1);
-         SSD1306_UpdateScreen();
+
+      ESP_ERROR_CHECK(ds3231_init_desc(&dev, 0, i2c_sda, i2c_scl));
+
+      // setup datetime: 2016-10-09 13:50:10
+      struct tm time = {
+      .tm_year = 122, //since 1900 (2016 - 1900)
+      .tm_mon  = 14,  // 0-based
+      .tm_mday = 26,
+      .tm_hour = 19,
+      .tm_min  = 7,
+      .tm_sec  = 0
+      };
+      ESP_ERROR_CHECK(ds3231_set_time(&dev, &time));
+
+      while (1)
+      {
+         float temp;
+
+         vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+         if (ds3231_get_temp_float(&dev, &temp) != ESP_OK)
+         {
+            printf("Could not get temperature\n");
+            continue;
+         }
+
+         if (ds3231_get_time(&dev, &time) != ESP_OK)
+         {
+            printf("Could not get time\n");
+            continue;
+         }
+
+         /* float is used in printf(). you need non-default configuration in
+         * sdkconfig for ESP8266, which is enabled by default for this
+         * example. see sdkconfig.defaults.esp8266
+         */
+         printf("%04d-%02d-%02d %02d:%02d:%02d, %.2f deg Cel\n", time.tm_year + 1900 /*Add 1900 for better readability*/, time.tm_mon + 1,
+            time.tm_mday, time.tm_hour, time.tm_min, time.tm_sec, temp);
       }
-   }
-
 }
 
 void app_main(void)
 {
-   myQueue = xQueueCreate(2, sizeof(temp_t));
-   
-   xTaskCreate(&task_one, "task 1", 1024, NULL, tskIDLE_PRIORITY, NULL);
-   xTaskCreate(&task_two, "task 2", 1024, NULL, tskIDLE_PRIORITY, NULL);
-   xTaskCreate(&ledTask, "LED task", 1024, NULL, 7, NULL);
-   xTaskCreate(&buttonTask, "Button task", 1024, NULL, tskIDLE_PRIORITY, NULL);
+      ESP_ERROR_CHECK(i2cdev_init());
    xTaskCreate(&lcdTask, "LCD task", 2048, NULL, 5, NULL);
-   xTaskCreate(&bmp180Task, "BMP180 Task", 2048, NULL, 5, NULL);
-   xTaskCreate(&ssd1306Task, "OLED Task", 2048, NULL, 3, NULL);
+   xTaskCreate(&bmp180Task, "BMP180 Task", 1920, NULL, 8, NULL);
+   xTaskCreate(&rtcTask, "RTC Task", 2048, NULL, 10, NULL);
 
 }
